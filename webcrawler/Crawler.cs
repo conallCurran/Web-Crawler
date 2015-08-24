8cd084a15a9c8469e22953c54fb1b19cdde57b53
Conall Curran <conallcurran@hotmail.com>
2015-08-24 15:29:52 +0100

    first commit webcrawler

---
+++ webcrawler/Crawler.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace webcrawler
+{
+    class Crawler
+    {
+        public class Crawler
+        {
+            public static void Main()
+            {
+                var crawler = new Crawler
+                {
+                    ExcludeFilters = new IExcludeFilter[]
+                    {
+                new ExcludeImagesFilter(),
+                new ExcludeTrackbacks(),
+                new ExcludeMailTo(),
+                new ExcludeHostsExcept(new[] { "nyqui.st" }),
+                new ExcludeJavaScript(),
+                new ExcludeAnchors(),
+                    }
+                };
+
+                crawler.OnCompleted += () =>
+                {
+                    Console.WriteLine("[Main] Crawl completed!");
+                    Environment.Exit(0);
+                };
+
+                crawler.OnPageDownloaded += page =>
+                {
+                    Console.WriteLine("[Main] Downloaded page {0}", page.Url);
+
+                    // Write external links
+                    foreach (var link in page.Links)
+                    {
+                        if (link.TargetUrl.Host != page.Url.Host)
+                        {
+                            Console.WriteLine("Found outbound link from {0} to {1}", page.Url, link.TargetUrl);
+                        }
+                    }
+                };
+
+                crawler.Enqueue(new Uri("http://nyqui.st"));
+                crawler.Start();
+
+                Console.WriteLine("[Main] Crawler started.");
+                Console.WriteLine("[Main] Press [enter] to abort.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ webcrawler/WebPage.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace Examples.System.Net
+{
+    public class WebRequestGetExample
+    {
+        public static void Main()
+        {
+            // Create a request for the URL. 
+            WebRequest request = WebRequest.Create("http://www.conallcurran.com");
+
+            // If required by the server, set the credentials.
+            request.Credentials = CredentialCache.DefaultCredentials;
+
+            // Get the response.
+            WebResponse response = request.GetResponse();
+
+            // Display the status.
+            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+
+            // Get the stream containing content returned by the server.
+            Stream dataStream = response.GetResponseStream();
+
+            // Open the stream using a StreamReader for easy access.
+            StreamReader reader = new StreamReader(dataStream);
+
+            // Read the content.
+            string responseFromServer = reader.ReadToEnd();
+
+            // Display the content.
+            Console.WriteLine(responseFromServer);
+
+            //write console out put to a file.
+            StreamWriter SW;
+            SW = File.CreateText("C:\\Users\\Conal_Curran\\OneDrive\\C#\\MyProjects\\Web Crawler\\webcrawler\\response.htm");
+            SW.WriteLine(responseFromServer);
+
+
+            // Clean up the streams and the response.
+            reader.Close();
+            response.Close();
+
+
+            Console.Read();
+        }
+    }
+}
